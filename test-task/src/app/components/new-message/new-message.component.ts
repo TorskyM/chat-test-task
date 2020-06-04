ff889eafa443980e27863917900d2c0fec4b64cd
Mikhailo <mikhailotorsky@gmail.com>
2020-06-05 00:41:20 +0300

    start iplementing post message feature

--- test-task/src/app/components/new-message/new-message.component.ts
+++ test-task/src/app/components/new-message/new-message.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpService } from '../../services/http.service'
+import { IMessage } from 'src/app/interfaces/message.interface';
+
 @Component({
   selector: 'app-new-message',
   templateUrl: './new-message.html',
   styleUrls: ['./new-message.scss']
 })
 export class NewMessageComponent implements OnInit {
-  public sendImageUrl = '../../../assets/images/send-icon.png'
 
+  constructor(private readonly httpService: HttpService) { }
+
+  public sendImageUrl = '../../../assets/images/send-icon.png'
+  public newMessageText: string = '';
+  public chatСorrespondence: Array<IMessage> = [];
 
   ngOnInit(): void {
   }
 
+  public onKeyDown(event): void {
+    if (event.key === 'Enter') {
+      this.sendMessage();
+    }
+  }
+
+  public sendMessage(): void {
+    if (this.newMessageText.length) {
+      const newMessage = {
+        isOwnMessage: true,
+        content: this.newMessageText,
+      };
+
+      this.newMessageText = '';
+
+      this.httpService.getJoke().subscribe(el => {
+        this.chatСorrespondence.push({
+          content: el.value,
+          createdAt: new Date().toString(),
+          isOwnMessage: false,
+          isTimeStamped: true
+        });
+      })
+    }
+  }
 }
